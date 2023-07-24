5381c87d51008f0c478def421768245ae659c3c0
guilleto98 <guillermo98td@gmail.com>
2023-07-24 19:18:26 -0300

    Details about the procces

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const factElements = document.querySelectorAll('.factDescription');
-    factElements.forEach((element, index) => {
+    
+    const factElements = document.querySelectorAll('.factDescription'); // Here I save the elements with class factDescription
+
+    factElements.forEach((element, index) => { // For each element in the factElements array, this loop iterates over the elements.
+        
         fetch('https://catfact.ninja/fact')
         .then(response => response.json())
         .then(data => {
