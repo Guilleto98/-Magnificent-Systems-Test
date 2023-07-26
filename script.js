09006bd1e63394a5f031411ceaee10caba98a83e
guilleto98 <guillermo98td@gmail.com>
2023-07-26 19:23:49 -0300

    Details test 2

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-    const factElements = document.querySelectorAll('.factDescription'); // Here I save the elements with class factDescription
+  const factElements = document.querySelectorAll('.factDescription');
 
-    factElements.forEach((element, index) => { // For each element in the factElements array, this loop iterates over the elements.
-        
-        fetch('https://catfact.ninja/fact')
-        .then(response => response.json())
-        .then(data => {
-          element.textContent = `Fact ${index + 1}: ${data.fact}`;
-        })
-        .catch(error => {
-          console.log('Error al obtener el hecho sobre gatos:', error);
-        });
+  factElements.forEach((element) => {
+    element.addEventListener('click', async () => {
+      const factItem = element.parentElement;
+      const activeItem = document.querySelector('.factItem.active');
+
+      if (activeItem && activeItem !== factItem) {
+        activeItem.classList.remove('active');
+        const additionalInfo = activeItem.querySelector('.additionalInfo');
+        const accordionContainer = factItem.querySelector('.accordionContainer');
+        const apiText = factItem.querySelector('.apiText');
+        apiText.classList.remove('active');
+        accordionContainer.classList.remove('active');
+        additionalInfo.classList.remove('active');
+        const accordionText = activeItem.querySelector('.accordionText');
+        accordionText.textContent = 'Click to open fact';
+        const accordionSign = activeItem.querySelector('.accordionSign');
+        accordionSign.textContent = '+';
+      }
+
+      factItem.classList.toggle('active');
+      const additionalInfo = factItem.querySelector('.additionalInfo');
+      const accordionContainer = factItem.querySelector('.accordionContainer');
+      const apiText = factItem.querySelector('.apiText');
+      additionalInfo.classList.toggle('active');
+      accordionContainer.classList.toggle('active');
+      apiText.classList.toggle('active'); 
+      const accordionText = factItem.querySelector('.accordionText');
+      const accordionSign = factItem.querySelector('.accordionSign');
+
+      if (additionalInfo.classList.contains('active')) {
+        const response = await fetch('https://catfact.ninja/fact');
+        const data = await response.json();
+        const apiText = factItem.querySelector('.apiText');
+        apiText.textContent = data.fact;
+        accordionText.textContent = 'Click to close fact';
+        accordionSign.textContent = '-';
+      } else {
+        accordionText.textContent = 'Click to open fact';
+        accordionSign.textContent = '+';
+      }
     });
-  });   
+  });
+});
